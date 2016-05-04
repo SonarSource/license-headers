d66452a35f2f448ff0f2c230b16666554885ac9f
David Rautureau <david.rautureau@sonarsource.com>
2016-05-04 17:31:55 +0200

    no dory analysis

--- travis.sh
+++ travis.sh
@@ -10,4 +10,18 @@ function installTravisTools {
 
 installTravisTools
 
-regular_mvn_build_deploy_analyze
+# get current version from pom
+CURRENT_VERSION=`maven_expression "project.version"`
+
+if [[ $CURRENT_VERSION =~ "-SNAPSHOT" ]]; then
+  echo "======= Found SNAPSHOT version ======="
+  # Do not deploy a SNAPSHOT version but the release version related to this build
+  set_maven_build_version $TRAVIS_BUILD_NUMBER
+else
+  echo "======= Found RELEASE version ======="
+fi
+
+export MAVEN_OPTS="-Xmx1536m -Xms128m"
+mvn deploy \
+    -Pdeploy-sonarsource,release \
+    -B -e -V
