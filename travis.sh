7e445a6c7919d4459410c821e713e9f3cb0c7be1
David Rautureau <david.rautureau@sonarsource.com>
2019-03-04 09:23:00 +0100

    Use JDK8 (#5)

--- travis.sh
+++ travis.sh
@@ -3,12 +3,13 @@
 set -euo pipefail
 
 function installTravisTools {
-  mkdir ~/.local
+  mkdir -p ~/.local
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v54 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
 installTravisTools
+. installJDK8
 
 . set_maven_build_version $TRAVIS_BUILD_NUMBER
 
